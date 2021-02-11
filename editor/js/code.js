33e91a2d60ee11e594663242a1bef60970d3f6a7
FaustinM <faustino40@outlook.fr>
2021-02-11 16:13:29 +0100

    Ouverture de la branch gh-pages

--- editor/js/code.js
+++
@@ -1,6 +0,0 @@
-LiteGraph.allow_scripts = true;
-
-const editor = new LiteGraph.Editor("main", { miniwindow: false });
-window.graphcanvas = editor.graphcanvas;
-window.graph = editor.graph;
-window.addEventListener("resize", () => { editor.graphcanvas.resize(); });
